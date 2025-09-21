c48b200cceece0a0ad5fa993ecaf5da919ed6301
kasra Frp <142843648+Msaeidfrp60@users.noreply.github.com>
2025-09-21 15:54:40 +0330

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,44 +1,40 @@
+import React from ‘react’;
+import { BrowserRouter as Router, Routes, Route, Navigate } from ‘react-router-dom’;
+import Login from ‘./components/Login.tsx’;
+import Dashboard from ‘./components/Dashboard.tsx’;
+import Support from ‘./components/Support.tsx’;
+import Subscription from ‘./components/Subscription.tsx’;
+import Profile from ‘./components/Profile.tsx’;
+import { UserProvider, useUser } from ‘./contexts/UserContext’;
 
-import React from 'react';
-import { HashRouter, Routes, Route, Navigate, Outlet } from 'react-router-dom';
-import Login from './components/Login';
-import Dashboard from './components/Dashboard';
-import Support from './components/Support';
-import Subscription from './components/Subscription';
-import Profile from './components/Profile';
-import { UserProvider, useUser } from './contexts/UserContext';
-
-const ProtectedRoute: React.FC = () => {
-    const { user } = useUser();
-    return user.isLoggedIn ? <Outlet /> : <Navigate to="/login" replace />;
+const ProtectedRoute: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+const { user } = useUser();
+return user.isLoggedIn ? <>{children}</> : <Navigate to="/login" replace />;
 };
 
 const AppRoutes: React.FC = () => {
-  return (
-    <div className="bg-white text-black min-h-screen">
-      <HashRouter>
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route element={<ProtectedRoute />}>
-            <Route path="/" element={<Dashboard />} />
-            <Route path="/support" element={<Support />} />
-            <Route path="/subscription" element={<Subscription />} />
-            <Route path="/profile" element={<Profile />} />
-          </Route>
-          <Route path="*" element={<Navigate to="/" replace />} />
-        </Routes>
-      </HashRouter>
-    </div>
-  );
+return (
+<div className="bg-white text-black min-h-screen">
+<HashRouter>
+<Routes>
+<Route path=”/login” element={<Login />} />
+<Route path=”/” element={<ProtectedRoute><Dashboard /></ProtectedRoute>} />
+<Route path=”/support” element={<Support />} />
+<Route path=”/subscription” element={<Subscription />} />
+<Route path=”/profile” element={<Profile />} />
+<Route path=”*” element={<Navigate to="/" replace />} />
+</Routes>
+</HashRouter>
+</div>
+);
 };
 
-
 const App: React.FC = () => {
-  return (
-    <UserProvider>
-      <AppRoutes />
-    </UserProvider>
-  );
-}
+return (
+<UserProvider>
+<AppRoutes />
+</UserProvider>
+);
+};
 
 export default App;
