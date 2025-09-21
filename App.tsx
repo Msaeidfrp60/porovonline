a93f33b4ce19b43c33444594f84a4ee057e7df74
kasra Frp <142843648+Msaeidfrp60@users.noreply.github.com>
2025-09-21 16:09:01 +0330

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,38 +1,27 @@
 import React from ‘react’;
 import { BrowserRouter as Router, Routes, Route, Navigate } from ‘react-router-dom’;
-import Login from ‘./components/Login.tsx’;
-import Dashboard from ‘./components/Dashboard.tsx’;
-import Support from ‘./components/Support.tsx’;
-import Subscription from ‘./components/Subscription.tsx’;
-import Profile from ‘./components/Profile.tsx’;
-import { UserProvider, useUser } from ‘./contexts/UserContext’;
+import Login from ‘./components/Login’;
+import Dashboard from ‘./components/Dashboard’;
+import Support from ‘./components/Support’;
+import Subscription from ‘./components/Subscription’;
+import Profile from ‘./components/Profile’;
+import { UserProvider } from ‘./contexts/UserContext’;
 
-const ProtectedRoute: React.FC<{ children: React.ReactNode }> = ({ children }) => {
-const { user } = useUser();
-return user.isLoggedIn ? <>{children}</> : <Navigate to="/login" replace />;
-};
-
-const AppRoutes: React.FC = () => {
+const App: React.FC = () => {
 return (
+<UserProvider>
+<Router>
 <div className="bg-white text-black min-h-screen">
-<HashRouter>
 <Routes>
 <Route path=”/login” element={<Login />} />
-<Route path=”/” element={<ProtectedRoute><Dashboard /></ProtectedRoute>} />
+<Route path=”/” element={<Dashboard />} />
 <Route path=”/support” element={<Support />} />
 <Route path=”/subscription” element={<Subscription />} />
 <Route path=”/profile” element={<Profile />} />
 <Route path=”*” element={<Navigate to="/" replace />} />
 </Routes>
-</HashRouter>
 </div>
-);
-};
-
-const App: React.FC = () => {
-return (
-<UserProvider>
-<AppRoutes />
+</Router>
 </UserProvider>
 );
 };
