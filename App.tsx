c7a8511a99ab039d781ee8299635244244ea4c41
kasra Frp <142843648+Msaeidfrp60@users.noreply.github.com>
2025-09-21 18:11:52 +0330

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,29 +1,28 @@
-import React from ‘react’;
-import { BrowserRouter as Router, Routes, Route, Navigate } from ‘react-router-dom’;
-import Login from ‘./components/Login’;
-import Dashboard from ‘./components/Dashboard’;
-import Support from ‘./components/Support’;
-import Subscription from ‘./components/Subscription’;
-import Profile from ‘./components/Profile’;
-import { UserProvider } from ‘./contexts/UserContext’;
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import Login from "./components/Login";
+import Dashboard from "./components/Dashboard";
+import Support from "./components/Support";
+import Subscription from "./components/Subscription";
+import UserProvider from "./contexts/UserContext";
 
 const App: React.FC = () => {
-return (
-<UserProvider>
-<Router>
-<div className="bg-white text-black min-h-screen">
-<Routes>
-<Route path=”/login” element={<Login />} />
-<Route path=”/” element={<Dashboard />} />
-<Route path=”/support” element={<Support />} />
-<Route path=”/subscription” element={<Subscription />} />
-<Route path=”/profile” element={<Profile />} />
-<Route path=”*” element={<Navigate to="/" replace />} />
-</Routes>
-</div>
-</Router>
-</UserProvider>
-);
+  return (
+    <UserProvider>
+      <Router>
+        <div className="min-h-screen">
+          <Routes>
+            <Route path="/" element={<Navigate to="/login" replace />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/support" element={<Support />} />
+            <Route path="/subscription" element={<Subscription />} />
+            <Route path="*" element={<div>Page not found</div>} />
+          </Routes>
+        </div>
+      </Router>
+    </UserProvider>
+  );
 };
 
 export default App;
