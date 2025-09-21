e17933a2cb4d5f5b469a3367e2e8de94a93e9a9a
kasra Frp <142843648+Msaeidfrp60@users.noreply.github.com>
2025-09-21 18:22:29 +0330

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,27 +1,22 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
-import Login from "./components/login";
+
+import Login from "./components/Login";
 import Dashboard from "./components/Dashboard";
 import Support from "./components/Support";
 import Subscription from "./components/Subscription";
-import UserProvider from "./contexts/UserContext";
 
 const App: React.FC = () => {
   return (
-    <UserProvider>
-      <Router>
-        <div className="min-h-screen">
-          <Routes>
-            <Route path="/" element={<Navigate to="/login" replace />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/dashboard" element={<Dashboard />} />
-            <Route path="/support" element={<Support />} />
-            <Route path="/subscription" element={<Subscription />} />
-            <Route path="*" element={<div>Page not found</div>} />
-          </Routes>
-        </div>
-      </Router>
-    </UserProvider>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Navigate to="/login" replace />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/support" element={<Support />} />
+        <Route path="/subscription" element={<Subscription />} />
+      </Routes>
+    </Router>
   );
 };
 
